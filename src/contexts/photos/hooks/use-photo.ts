3a53311661c8cf62273e5f419d6d0aaa977209f9
otavio <otaviotemoteo@gmail.com>
2026-02-03 19:34:14 -0300

    feat: add toast when create a photo

--- src/contexts/photos/hooks/use-photo.ts
+++ src/contexts/photos/hooks/use-photo.ts
@@ -2,6 +2,7 @@ import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { api, fetcher } from "../../../helpers/api";
 import type { Photo } from "../models/photo";
 import type { PhotoNewFormSchema } from "../../schemas";
+import { toast } from "sonner";
 
 interface PhotoDetailResponse extends Photo {
   nextPhotoId?: string;
@@ -17,7 +18,7 @@ export default function usePhoto(id?: string) {
   const queryClient = useQueryClient();
 
   async function createPhoto(payload: PhotoNewFormSchema) {
-    // eslint-disable-next-line no-useless-catch
+
     try {
       const { data: photo } = await api.post<Photo>("/photos", {
         title: payload.title,
@@ -42,7 +43,10 @@ export default function usePhoto(id?: string) {
       }
 
       queryClient.invalidateQueries({ queryKey: ["photos"] });
+
+      toast.success("Foto criada com sucesso");
     } catch (error) {
+      toast.error("Erro ao criar foto");
       throw error;
     }
   }
